378e603549c558a95f181693909c6aa8f74846de
fuad1 <mfuad703@gmail.com>
2019-12-15 18:27:26 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/polinema/tiketbus/activities/MainActivity.java
@@ -0,0 +1,61 @@
+package id.ac.polinema.tiketbus.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import id.ac.polinema.tiketbus.R;
+import id.ac.polinema.tiketbus.fragments.Akun;
+import id.ac.polinema.tiketbus.fragments.BantuanFragment;
+import id.ac.polinema.tiketbus.fragments.BerandaFragment;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        loadFragment(new BerandaFragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        // beri listener pada saat item/menu bottomnavigation terpilih
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .addToBackStack(null)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.BerandaFragment:
+                fragment = new BerandaFragment();
+                break;
+            case R.id.BantuanFragment:
+                fragment = new BantuanFragment();
+                break;
+            case R.id.akun:
+                fragment = new Akun();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}
---
+++ app/src/main/java/id/ac/polinema/tiketbus/fragments/Akun.java
@@ -0,0 +1,29 @@
+package id.ac.polinema.tiketbus.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import id.ac.polinema.tiketbus.R;
+
+
+public class Akun extends Fragment {
+
+    public Akun() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_akun, container, false);
+    }
+
+}
