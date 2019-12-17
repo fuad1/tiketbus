ca5c9c4c385b7e44376ad542e4229de02a158f0d
Muchammad Nur Rizki <rizkiinc08@gmail.com>
2019-12-17 15:23:19 +0700

    Fragmen Akun

--- app/src/main/java/id/ac/polinema/tiketbus/fragments/Akun.java
+++ app/src/main/java/id/ac/polinema/tiketbus/fragments/Akun.java
@@ -11,19 +11,46 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import id.ac.polinema.tiketbus.R;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-
-public class Akun extends Fragment {
-
+public class Akun  extends Fragment {
+    EditText inuser,inpass;
+    Button btnlgn;
     public Akun() {
         // Required empty public constructor
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_akun, container, false);
+        View view =  inflater.inflate(R.layout.fragment_akun, container, false);
+        inuser  = view.findViewById(R.id.inpass);
+        inpass  = view.findViewById(R.id.inpass);
+        btnlgn = view.findViewById(R.id.btnlgn);
+
+        btnlgn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user_name = inuser.getText().toString();
+                String pass =  inpass.getText().toString();
+                if (user_name.equals("admin")&&pass.equals("admin")){
+                    Toast.makeText(getActivity(),"Username dan Password benar Anda berhasil Login",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getActivity(),"Username dan Pssword tidak sesuai Anda gagal Login",Toast.LENGTH_SHORT).show();
+                }
+            }
+
+
+        });
+        return view;
     }
 
+
 }
+
+
